c2dc7aa7230c765460a7391cda2f5acc46c839d5
marlon.dsn.pack <marlon.dsn.pack@alterdata.com.br>
2021-07-23 19:46:42 -0300

    capítulo um concluído

--- Alura.Filmes/Alura.Filmes.App/Program.cs
+++ Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Alura.Filmes.App
+﻿using Alura.Filmes.App.Dados;
+
+namespace Alura.Filmes.App
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            
+            using (var contexto = new AluraFilmesContexto())
+            {
+                foreach (var ator in contexto.Atores)
+                {
+                    System.Console.WriteLine(ator);
+                }
+
+            }
         }
     }
 }
