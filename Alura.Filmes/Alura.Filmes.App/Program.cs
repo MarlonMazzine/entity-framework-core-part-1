e1132e1e3d9dae8104d25df6a7d075514c85a367
marlon.dsn.pack <marlon.dsn.pack@alterdata.com.br>
2021-08-12 08:58:38 -0300

    finalizando aula 6 parte 2 e curso

--- Alura.Filmes/Alura.Filmes.App/Program.cs
+++ Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -1,9 +1,8 @@
 ﻿using Alura.Filmes.App.Dados;
 using Alura.Filmes.App.Extensions;
-using Alura.Filmes.App.Negocio;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Linq;
+using System.Data.SqlClient;
 
 namespace Alura.Filmes.App
 {
@@ -14,17 +13,37 @@ namespace Alura.Filmes.App
             using (var contexto = new AluraFilmesContexto())
             {
                 contexto.LogSQLToConsole();
-                var sql = @"select a.* from actor a
-                    inner join top5_most_starred_actors filmes on filmes.actor_id = a.actor_id";
 
-                var atoresMaisAtuantes = contexto.Atores.FromSql(sql)
-                    .Include(a => a.Filmografia);
+                var sql = "INSERT INTO language (name) VALUES ('Teste 1'), ('Teste 2'), ('Teste 3')";
+                var registros = contexto.Database.ExecuteSqlCommand(sql);
+                Console.WriteLine($"O total de registros afetados é {registros}.");
 
-                foreach (var ator in atoresMaisAtuantes)
-                {
-                    Console.WriteLine($"O ator {ator.PrimeiroNome} {ator.UltimoNome} atuou em {ator.Filmografia.Count} filmes");
-                }
+                var deleteSql = "DELETE FROM language WHERE name LIKE 'Teste%'";
+                registros = contexto.Database.ExecuteSqlCommand(deleteSql);
+                Console.WriteLine($"O total de registros afetados é {registros}.");
             }
+        }
+
+        static void StoredProcedure(DbContext contexto)
+        {
+            var categ = "Action"; //36
+
+            var paramCateg = new SqlParameter("category_name", categ);
+
+            var paramTotal = new SqlParameter
+            {
+                ParameterName = "@total_actors",
+                Size = 4,
+                Direction = System.Data.ParameterDirection.Output
+            };
+
+            contexto.Database
+                .ExecuteSqlCommand(
+                "total_actors_from_giver_category @category_name, @total_actors OUT",
+                    paramCateg,
+                    paramTotal);
+
+            System.Console.WriteLine($"O total de atores na categoria {categ} é de {paramTotal.Value}");
         }
     }
 }
