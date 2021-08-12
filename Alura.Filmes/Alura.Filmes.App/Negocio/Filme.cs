fdc0219f752e0c19a632b5439f1e2044cdb3a5cc
marlon.dsn.pack <marlon.dsn.pack@alterdata.com.br>
2021-08-12 07:45:02 -0300

    finalizando capítulo 3 parte 2

--- Alura.Filmes/Alura.Filmes.App/Negocio/Filme.cs
+++ Alura.Filmes/Alura.Filmes.App/Negocio/Filme.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+using Alura.Filmes.App.Extensions;
 
 namespace Alura.Filmes.App.Negocio
 {
@@ -10,7 +10,15 @@ namespace Alura.Filmes.App.Negocio
         public string Descricao { get; set; }
         public string AnoLancamento { get; set; }
         public short Duracao { get; set; }
-        public string Classificacao { get; set; }
+
+        public string TextoClassificacao { get; private set; }
+
+        public ClassificacaoIndicativa Classificacao
+        {
+            get { return TextoClassificacao.ParaValor(); }
+            set { TextoClassificacao = value.ParaString(); }
+        }
+
         public IList<FilmeAtor> Atores { get; set; }
         public Idioma IdiomaFalado { get; set; }
         public Idioma IdiomaOriginal { get; set; }
